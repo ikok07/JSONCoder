0c29c341a718981c96bc183c3881ea0a7c56f385
ikok07 <kokmarok@gmail.com>
2023-12-11 15:55:11 +0200

    develop encode and decode

--- Sources/JSONCoder/JSONCoder.swift
+++ Sources/JSONCoder/JSONCoder.swift
@@ -1,2 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+struct JSONCoder {
+    
+    static func decode<T: Codable>(_ data: Data) -> T? {
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            print("Error decoding data: \(error)")
+            return nil
+        }
+    }
+    
+    static func encode(_ data: Data) -> Data? {
+        do {
+            return try JSONEncoder().encode(data)
+        } catch {
+            print("Error encoding data: \(error)")
+            return nil
+        }
+    }
+    
+    
+}
