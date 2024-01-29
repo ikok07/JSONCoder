6e4a51f37443cbec204b70dc233723f643157b58
ikok07 <kokmarok@gmail.com>
2024-01-29 18:12:35 +0200

    add serialize method

--- Sources/JSONCoder/JSONCoder.swift
+++ Sources/JSONCoder/JSONCoder.swift
@@ -23,4 +23,22 @@ public struct JSONCoder {
         }
     }
     
+    public static func serialize(_ data: Data) -> [String: Any]? {
+        do {
+            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                var dic: [String: Any] = [:]
+                for (key, value) in json {
+                    print("\(key): \(value)")
+                    dic[key] = value
+                }
+                return dic
+            } else {
+                print("Failed to serialize JSON data!")
+            }
+        } catch {
+            print("Failed to serialize JSON data!: \(error)")
+        }
+        return nil
+    }
+    
 }
